33ad903216061fa1ad180cc350ab19472d2ccaa3
VDotMint <uts.lvm.technology@gmail.com>
2023-11-29 16:55:54 +0700

    Added 3 holiday sign recognition models, requirement file

---
+++ camera.py
@@ -0,0 +1,101 @@
+import cv2
+import tensorflow
+import numpy as np
+import mediapipe as mp
+import model.model_loader as md_loader
+
+mp_holistic = mp.solutions.holistic
+mp_drawing = mp.solutions.drawing_utils
+colors = [(255,0,0), (0,255,0), (0,0,255), (255,255,0), (255,0,255), (0,255,255), (0,0,0)]
+
+def mediapipe_detection(image, model):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) 
+    image.flags.writeable = False                  
+    results = model.process(image)             
+    image.flags.writeable = True                
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR) 
+    return image, results
+
+def draw_styled_landmarks(image, results):
+    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
+                            mp_drawing.DrawingSpec(color=(80,22,10), thickness=2, circle_radius=4), 
+                            mp_drawing.DrawingSpec(color=(80,44,121), thickness=2, circle_radius=2)
+                        ) 
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
+                            mp_drawing.DrawingSpec(color=(121,22,76), thickness=2, circle_radius=4), 
+                            mp_drawing.DrawingSpec(color=(121,44,250), thickness=2, circle_radius=2)
+                        ) 
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
+                            mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4), 
+                            mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)
+                        ) 
+    
+def extract_keypoints(results):
+    pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(33*4)
+    lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
+    rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
+    return np.concatenate([pose, lh, rh])
+
+def prob_viz(res, actions, input_frame, colors):
+    output_frame = input_frame.copy()
+    for num, prob in enumerate(res):
+        cv2.rectangle(output_frame, (0,60+num*40), (int(prob*100), 90+num*40), colors[num], -1)
+        cv2.putText(output_frame, str(actions[num]), (0, 85+num*40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
+        
+    return output_frame
+
+def convert_to_str(items):
+    return [convert_to_str(s) if isinstance(s, list) else str(s) for s in items ]
+
+model = md_loader.load_model(md_loader.model_catalogue["gpmn"])
+actions = convert_to_str(list(range(0, md_loader.model_catalogue["gpmn"]["no_of_states"])))
+
+sequence = []
+sentence = []
+predictions = []
+threshold = 0.8
+
+# cap = cv2.VideoCapture("test2.mp4")
+cap = cv2.VideoCapture(0)
+with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+    while cap.isOpened():
+        ret, frame = cap.read()
+
+        image, results = mediapipe_detection(frame, holistic)
+        print(results)
+
+        draw_styled_landmarks(image, results)
+        
+        keypoints = extract_keypoints(results)
+        sequence.append(keypoints)
+        sequence = sequence[-5:]
+        
+        if len(sequence) == 5:
+            res = model.predict(np.expand_dims(sequence, axis=0))[0]
+            print(actions[np.argmax(res)])
+            predictions.append(np.argmax(res))
+            
+            if res[np.argmax(res)] > threshold: 
+                if np.unique(predictions[-3:])[0]==np.argmax(res): 
+                    if len(sentence) > 0: 
+                        if actions[np.argmax(res)] != sentence[-1] and np.argmax(res) != 0 and actions[np.argmax(res)] not in sentence:
+                            sentence.append(actions[np.argmax(res)])
+                    else:
+                        if np.argmax(res) != 0:
+                            sentence.append(actions[np.argmax(res)])
+
+            if len(sentence) > 5: 
+                sentence = sentence[-5:]
+
+            image = prob_viz(res, actions, image, colors)
+            
+        cv2.rectangle(image, (0,0), (640, 40), (245, 117, 16), -1)
+        cv2.putText(image, ' '.join(sentence), (3,30), 
+                       cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+        
+        cv2.imshow('OpenCV Feed', image)
+        
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
---
+++ model/model_loader.py
@@ -0,0 +1,37 @@
+import numpy as np
+import mediapipe as mp
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense
+from tensorflow.keras.callbacks import TensorBoard
+
+model_catalogue = {
+    "qtpn": {
+        "path": 'model/holiday/qtpn.h5',
+        "no_of_states": 5
+    },
+    "gptd": {
+        "path": 'model/holiday/gptd.h5',
+        "no_of_states": 5
+    },
+    "gpmn": {
+        "path": 'model/holiday/gpmn.h5',
+        "no_of_states": 5
+    }
+}
+
+def load_model(model_name: dict):
+    """
+    Load TensorFlow model of the desired action
+    """
+    model = Sequential()
+    model.add(LSTM(64, return_sequences=True, activation='relu', input_shape=(5,258)))
+    model.add(LSTM(128, return_sequences=True, activation='relu'))
+    model.add(LSTM(64, return_sequences=False, activation='relu'))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dense(32, activation='relu'))
+    model.add(Dense(model_name["no_of_states"], activation='softmax'))
+
+    model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
+    model.load_weights(model_name["path"])
+
+    return model
